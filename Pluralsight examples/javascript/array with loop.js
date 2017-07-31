107b837cad58014cc857c82c55d66e4d99cdbff7
jai sharma <jaiomsharma@gmail.com>
2017-07-31 14:08:28 +0530

    array with loop

---
+++ Pluralsight examples/javascript/array with loop.js
@@ -0,0 +1,7 @@
+var friends = ["a" ,"b" ,"c" ,"d" ,"e" ,"f"];
+function greeting(){
+   for(var i = 0; i < friends.length; i++){
+      console.log("hello there " + friends[i]);
+   }
+}
+greeting();
